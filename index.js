259ea7710edba5196a39cae614a81e2796a31fab
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-07-23 05:00:51 +0900

    change font family

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ lewagon.addEventListener('click', ()=>{
   lwc.style.display = "block";
   cs50.style="opacity: 0.6";
   lewagon.style="opacity: 1"
+  lewagon.style="font-family: 'Pacifico', cursive"
 });
 
 
@@ -16,4 +17,5 @@ cs50.addEventListener('click', ()=>{
   lwc.style.display = "none";
   lewagon.style="opacity: 0.6"
   cs50.style="opacity: 1"
+  cs50.style="font-family: 'Pacifico', cursive"
 });
