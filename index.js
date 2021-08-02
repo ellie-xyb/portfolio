ca89c7bd593fc84d9c62181a00316c332e8d6023
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2021-08-02 22:21:05 +0900

    update education style

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const cs50 = document.getElementById('cs50');
-const lewagon = document.getElementById('lewagon');
-const csc = document.getElementById('cs50Card');
-const lwc = document.getElementById('lewagonCard');
+// const cs50 = document.getElementById('cs50');
+// const lewagon = document.getElementById('lewagon');
+// const csc = document.getElementById('cs50Card');
+// const lwc = document.getElementById('lewagonCard');
 
-lewagon.addEventListener('click', ()=>{
-  csc.style.display = "none";
-  lwc.style.display = "block";
-  cs50.style="opacity: 0.6";
-  lewagon.style="opacity: 1"
-  lewagon.style="font-family: 'Pacifico', cursive"
-});
+// lewagon.addEventListener('click', ()=>{
+//   csc.style.display = "none";
+//   lwc.style.display = "block";
+//   cs50.style="opacity: 0.6";
+//   lewagon.style="opacity: 1"
+//   lewagon.style="font-family: 'Pacifico', cursive"
+// });
 
 
-cs50.addEventListener('click', ()=>{
-  csc.style.display = "block";
-  lwc.style.display = "none";
-  lewagon.style="opacity: 0.6"
-  cs50.style="opacity: 1"
-  cs50.style="font-family: 'Pacifico', cursive"
-});
+// cs50.addEventListener('click', ()=>{
+//   csc.style.display = "block";
+//   lwc.style.display = "none";
+//   lewagon.style="opacity: 0.6"
+//   cs50.style="opacity: 1"
+//   cs50.style="font-family: 'Pacifico', cursive"
+// });
